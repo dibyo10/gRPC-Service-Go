32621e2b0eb99b113d2c04e053cd93c17a0cf5c8
Dibyo Chakraborty <dibyo.dc@gmail.com>
2026-02-28 02:24:33 +0530

    implemented http handler for orders and a util file for JSON parsing

--- services/orders/http.go
+++ services/orders/http.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	handler "github.com/dibyochakraborty/kitchen/services/orders/handler/orders"
+	"github.com/dibyochakraborty/kitchen/services/orders/service"
+)
+
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *httpServer {
+	return &httpServer{addr: addr}
+}
+
+func (s *httpServer) Run() error {
+	router := http.NewServeMux()
+
+	orderService := service.NewOrderService()
+	orderHandler := handler.NewHttpOrdersHandler(orderService)
+	orderHandler.RegisterRouter(router)
+
+	log.Println("Starting server on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
